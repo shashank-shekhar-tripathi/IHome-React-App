876f46dc7488061f42168af7107d5c5e072c3434
shashank-shekhar-tripathi <tripathiji931@gmail.com>
2024-12-22 00:27:37 +0530

    f

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,9 @@ import { motion } from "motion/react"
 
 function Header() {
   return (
-    <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden  ' style={ {backgroundImage: "url('/header_img.png')"}} id='Header'>
+    <div className='min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden  ' style={ {backgroundImage: "url('./public/header_img.png')"}} id='Header'>
    
+   {/*  */}
     <Navbar />
    
   <motion.div 
