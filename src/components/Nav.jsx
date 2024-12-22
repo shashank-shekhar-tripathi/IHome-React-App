f22c9ee431da99cc9cfb13daa33974cf73db3a6c
Shashank Shekhar Tripathi <165047919+shashank-shekhar-tripathi@users.noreply.github.com>
2024-12-22 09:36:55 +0530

    Update Nav.jsx

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,7 @@ function Nav() {
     <>
       <div className=" absolute top-0 left-0 w-full z-10 " id="Home">
         <div className=" container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg-transparent">
-         <img src={assets.logo} alt="img" className="  w-36    " />  
+         <img src="./public/newlogo.jpg" alt="img" className="  w-36    " />  
          
           <ul className="hidden md:flex gap-7 text-white">
             <a href="#Home" className="cursor-pointer hover:text-gray-400">
